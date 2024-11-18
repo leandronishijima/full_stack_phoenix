bd4b800db560279eb0bacf6d3430b000e62c8a75
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2024-11-17 22:25:42 -0300

    add(exercise): 17-slots

--- lib/heads_up_web/components/custom_components.ex
+++ lib/heads_up_web/components/custom_components.ex
@@ -15,4 +15,22 @@ defmodule HeadsUpWeb.CustomComponents do
     </div>
     """
   end
+
+  slot :inner_block, required: true
+  slot :tagline
+
+  def headline(assigns) do
+    assigns = assign(assigns, :emoji, ~w(👍 🙌 👊) |> Enum.random())
+
+    ~H"""
+    <div class="headline">
+      <h1>
+        <%= render_slot(@inner_block) %>
+      </h1>
+      <div :for={tag <- @tagline} class="tagline">
+        <%= render_slot(tag, @emoji) %>
+      </div>
+    </div>
+    """
+  end
 end
