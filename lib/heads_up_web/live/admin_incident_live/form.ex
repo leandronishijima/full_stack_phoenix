87e3ce229804d9945bf7d773e4436d0e7f6b05c5
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2025-01-12 21:57:23 -0300

    add(exercise): 34-form-with-changesets

--- lib/heads_up_web/live/admin_incident_live/form.ex
+++ lib/heads_up_web/live/admin_incident_live/form.ex
@@ -2,14 +2,16 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
   alias HeadsUp.Admin
   use HeadsUpWeb, :live_view
 
-  alias HeadsUp.Incidents.Incident
   alias HeadsUp.Admin
+  alias HeadsUp.Incidents.Incident
 
   def mount(_params, _session, socket) do
+    changeset = Incident.changeset(%Incident{}, %{})
+
     socket =
       socket
       |> assign(:page_title, "New Incident")
-      |> assign(:form, to_form(%{}, as: "incident"))
+      |> assign(:form, to_form(changeset))
 
     {:ok, socket}
   end
@@ -20,33 +22,43 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
       <%= @page_title %>
     </.header>
     <.simple_form for={@form} id="incident-form" phx-submit="save">
-      <.input field={@form["name"]} label="Name" />
-      <.input field={@form["priority"]} type="number" label="Priority" />
+      <.input field={@form[:name]} label="Name" />
+
+      <.input field={@form[:description]} type="textarea" label="Description" />
+
+      <.input field={@form[:priority]} type="number" label="Priority" />
+
       <.input
-        field={@form["status"]}
+        field={@form[:status]}
         type="select"
         label="Status"
         prompt="Choose a status"
         options={Ecto.Enum.values(Incident, :status)}
       />
-      <.input field={@form["description"]} type="textarea" label="Description" />
-      <.input field={@form["image_path"]} label="Image Path" />
+
+      <.input field={@form[:image_path]} label="Image Path" />
 
       <:actions>
         <.button phx-disable-with="Saving...">Save Incident</.button>
       </:actions>
     </.simple_form>
-    <.back navigate={~p"/admin/incidents"}>
-      Back
-    </.back>
+    <.back navigate={~p"/admin/incidents"}>Back</.back>
     """
   end
 
   def handle_event("save", %{"incident" => incident}, socket) do
-    _incident = Admin.create_incident(incident)
+    case Admin.create_incident(incident) do
+      {:ok, _incident} ->
+        socket =
+          socket
+          |> put_flash(:info, "Incident created successfully!")
+          |> push_navigate(to: ~p"/admin/incidents")
 
-    socket = push_navigate(socket, to: ~p"/admin/incidents")
+        {:noreply, socket}
 
-    {:noreply, socket}
+      {:error, %Ecto.Changeset{} = changeset} ->
+        socket = assign(socket, :form, to_form(changeset))
+        {:noreply, socket}
+    end
   end
 end
