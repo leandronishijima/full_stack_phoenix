bfbad621b629354b77124b2d18c826b08840f0f2
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2025-01-14 22:57:58 -0300

    add(exercise): 35-live-validation

--- lib/heads_up_web/live/admin_incident_live/form.ex
+++ lib/heads_up_web/live/admin_incident_live/form.ex
@@ -6,7 +6,7 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
   alias HeadsUp.Incidents.Incident
 
   def mount(_params, _session, socket) do
-    changeset = Incident.changeset(%Incident{}, %{})
+    changeset = Admin.change_incident(%Incident{})
 
     socket =
       socket
@@ -21,10 +21,10 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
     <.header>
       <%= @page_title %>
     </.header>
-    <.simple_form for={@form} id="incident-form" phx-submit="save">
+    <.simple_form for={@form} id="incident-form" phx-submit="save" phx-change="validate">
       <.input field={@form[:name]} label="Name" />
 
-      <.input field={@form[:description]} type="textarea" label="Description" />
+      <.input field={@form[:description]} type="textarea" label="Description" phx-debounce="blur" />
 
       <.input field={@form[:priority]} type="number" label="Priority" />
 
@@ -44,6 +44,14 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
     </.simple_form>
     <.back navigate={~p"/admin/incidents"}>Back</.back>
     """
+  end
+
+  def handle_event("validate", %{"incident" => incident_params}, socket) do
+    changeset = Admin.change_incident(%Incident{}, incident_params)
+
+    socket = assign(socket, :form, to_form(changeset, action: :validate))
+
+    {:noreply, socket}
   end
 
   def handle_event("save", %{"incident" => incident}, socket) do
